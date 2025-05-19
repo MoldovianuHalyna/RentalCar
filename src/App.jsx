b29c009435a16496a3a5e2b43338c04b2bf1d591
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-19 20:14:43 +0200

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home/Home";
+import Catalog from "./components/Catalog/Catalog";
+import CatalogItem from "./components/CatalogItem/CatalogItem";
 
 function App() {
-  return <h1>Rental Car App</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/catalog/:id" element={<CatalogItem />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
