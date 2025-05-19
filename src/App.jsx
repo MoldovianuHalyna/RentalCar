a5f36ea0c8ae15305f62fc0282e6f26aa54f423f
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-19 20:22:52 +0200

    navlink added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Catalog from "./pages/Catalog/Catalog";
 import CatalogItem from "./pages/CatalogItem/CatalogItem";
+import NotFound from "./components/NotFoundPage/NotFound";
 
 function App() {
   return (
     <div>
+      <div>
+        <NavLink to="/">Home Page</NavLink>
+        <NavLink to="/catalog"> Catalog </NavLink>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/catalog/:id" element={<CatalogItem />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
