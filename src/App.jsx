b34c3655371a4f68d369fc5c8866c4323e948af6
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-28 21:41:48 +0200

    provider fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { ToastContainer } from "react-toastify";
 import NotFound from "./components/NotFoundPage/NotFound";
 import Layout from "./components/Layout/Layout";
 import Loader from "./components/Loader/Loader";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
@@ -17,14 +15,13 @@ function App() {
   return (
     <Layout>
       <Suspense fallback={<Loader />}>
-        <Provider store={store}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/catalog/:id" element={<CatalogItemPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Provider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<CatalogItemPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
         <ToastContainer />
       </Suspense>
     </Layout>
